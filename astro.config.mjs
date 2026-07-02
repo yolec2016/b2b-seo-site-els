0e3a242ab017d916d8c3aefe5f42e13cec46aefb
yolec2016 <kyjm8rby4p@privaterelay.appleid.com>
2026-07-02 23:30:20 +0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://yoleckitchen.xyz', // 先填你的最终域名，或者暂时填 Netlify 域名
+
+  integrations: [
+    sitemap()
+  ]
+});
